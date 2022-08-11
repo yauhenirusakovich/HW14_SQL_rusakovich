51f41b89bb6192adc2e57228b25dff6946a7a78f
yauhenirusakovich <rusakovich2304@gmail.com>
2022-08-11 13:55:48 +0300

    final1

--- tasks/task4.sql
+++ tasks/task4.sql
@@ -1,4 +1,4 @@
 SELECT FirstName, LastName, Notes FROM Employees where LastName like 'Leverling' or FirstName like 'Leverling';
-SELECT Notes FROM Employees where BirthDate > 01/01/1960;
+SELECT Notes FROM Employees where BirthDate > 1960-01-01;
 SELECT BirthDate FROM Employees where FirstName like 'A+';
 SELECT FirstName, LastName, BirthDate FROM Employees order by BirthDate;
